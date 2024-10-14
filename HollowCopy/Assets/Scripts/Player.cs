cd0d6de1f1fdd9ea62486f24420492eeb2b2e47f
6TiRaN6 <40199@iitu.edu.kz>
2024-10-14 21:30:10 +0500

    Commite poka chto ne zakonchen

--- HollowCopy/Assets/Scripts/Player.cs
+++ HollowCopy/Assets/Scripts/Player.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float playerSpeed = 1.0f;
+    public float jumpForce = 1.0f;
+    private bool isGrounded;
     private Rigidbody2D rb;
+
 
     private void Awake()
     {
@@ -16,16 +19,22 @@ public class Player : MonoBehaviour
     {
         Vector2 movePlayer = new Vector2(0, 0);
 
+
         if (Input.GetKey(KeyCode.A))
         {
-            movePlayer.x = -0.01f;
+            movePlayer.x = -1f;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            movePlayer.x = 0.01f;
+            movePlayer.x = 1f;
         }
 
-        rb.MovePosition(movePlayer + rb.position);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        rb.MovePosition(movePlayer + rb.position * Time.fixedDeltaTime * playerSpeed);
     }
 }
